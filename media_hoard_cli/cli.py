40ea954508479c9aedf474a56a404c7f2092b531
Justin Stout <midwatch@jstout.us>
2021-12-01 16:15:29 -0800

    add failing tests

--- media_hoard_cli/cli.py
+++ media_hoard_cli/cli.py
@@ -5,14 +5,54 @@ import sys
 import click
 
 
-@click.command()
+@click.group()
 def main():
-    """Console script for media_hoard_cli."""
-    click.echo("Replace this message by putting your code into "
-               "media_hoard_cli.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    """Empty click anchor function."""
 
+@click.command()
+@click.option('--cfg-file', default="$HOME/.config/media_hoard/config.yaml",
+    help="path to config file")
+@click.argument('title')
+@click.argument('src')
+def add(cfg_file, title, src):
+    """Add a new file from local host.
+
+    TITLE: User friendly title
+    SRC: path to item to add (path/to/file.pdf)
+    """
+    # media_hoard add --chunk chunks.csv TITLE path/to/file.pdf
+
+    # print(title)        # Intersting item
+    # print(cfg_file)     # $HOME/.config/media_hoard/config.yaml
+    # print(src)          # file.pdf
+
+
+    # config file at $HOME/.config/media_hoard/config.{???}
+
+    # cfg.item_url = "http://stacks/{item.nid}/{item.name}"            # user supplied so we have to use
+                                                                # string template
+    # cfg.upload_dir = "justin@marauder/volume1/web/stacks"
+    # ctx.run(f'rsync -r work_dir config.upload_dir')
+
+    # cfg = hoard.load_config()
+    # item = hoard.new_item('title', 'path/to/file.pdf')
+
+    # item['nid']       # nano_id
+    # item['name']      # {title_slug}.{ext}
+    # item['src']       # path/to/file
+    # item['title']
+
+    # with tempdir as work_dir:
+    #     cp(item['src'], "work_dir/item['nid']/item['name']")
+    #     rsync('work_dir', "cfg.upload_dir/item['nid']")
+    #     # justin@marauder/volume1/web/stacks/{nid}/item_name.pdf
+
+    # print(f'{item.title}')
+    # print()
+    # print(f'- {cfg.item_url}')
+
+
+main.add_command(add)
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())  # pylint: disable=no-value-for-parameter
--- tests/test_media_hoard_cli.py
+++ tests/test_media_hoard_cli.py
@@ -7,14 +7,39 @@ import pytest
 from click.testing import CliRunner
 
 from media_hoard_cli import cli
+from media_hoard_cli import hoard
+
+NIDS = ['YKIKuCiQAl']
+def test_new_item():
+
+    # item = hoard.new_item('title', 'path/to/file.pdf')
+    item = hoard.new('Intersting Item', 'file.pdf')
+
+    assert item.nid == NIDS[0]
+    assert item.name == "interesting_item.pdf"
+    assert item.src == 'file.pdf'
+    assert item.title == 'Interesting Item'
 
 
-def test_command_line_interface():
-    """Test the CLI."""
+def test_cli_add():
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.main, ['add', 'Intesting Item', 'file.pdf'])
     assert result.exit_code == 0
-    assert 'media_hoard_cli.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+
+    expected = """Interesting Item
+
+  - http://localhost/YKIKuCiQAl/interesting_item.pdf
+
+"""
+
+    assert result.output == expected
+
+# def test_command_line_interface():
+#     """Test the CLI."""
+#     runner = CliRunner()
+#     result = runner.invoke(cli.main)
+#     assert result.exit_code == 0
+#     assert 'media_hoard_cli.cli.main' in result.output
+#     help_result = runner.invoke(cli.main, ['--help'])
+#     assert help_result.exit_code == 0
+#     assert '--help  Show this message and exit.' in help_result.output
